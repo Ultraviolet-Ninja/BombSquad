d4951f19971d1ca2f859fd1b8a82171c4942befa
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-01-20 17:48:05 -0600

    Added Alphabet and Astrology

--- src/main/java/bomb/selection/SelectionController.java
+++ src/main/java/bomb/selection/SelectionController.java
@@ -5,6 +5,8 @@ import java.util.EnumSet;
 public class SelectionController {
     private final EnumSet<Puzzle> puzzleSet;
 
+
+
     public SelectionController() {
         puzzleSet = EnumSet.noneOf(Puzzle.class);
     }
