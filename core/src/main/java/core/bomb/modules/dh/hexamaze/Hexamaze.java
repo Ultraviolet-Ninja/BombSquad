7247120e23c744599dc8844ce3b6bd26b7659bd3
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-02-08 08:24:05 -0600

    Fixed Hexamaze

--- core/src/main/java/core/bomb/modules/dh/hexamaze/Hexamaze.java
+++ core/src/main/java/core/bomb/modules/dh/hexamaze/Hexamaze.java
@@ -1,8 +1,22 @@
 package core.bomb.modules.dh.hexamaze;
 
+import core.bomb.Widget;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.maze_finding.MazeSearch;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.pathfinding.ExitChecker;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.pathfinding.MazeRunner;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.storage.Grid;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.storage.HexNode;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.storage.HexagonalPlane;
+import core.bomb.modules.dh.hexamaze.hexalgorithm.storage.Maze;
 import javafx.scene.paint.Color;
+import org.javatuples.Pair;
+import org.javatuples.Quartet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import tools.Coordinates;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static javafx.scene.paint.Color.BLUE;
@@ -12,7 +26,7 @@ import static javafx.scene.paint.Color.PINK;
 import static javafx.scene.paint.Color.RED;
 import static javafx.scene.paint.Color.YELLOW;
 
-public class Hexamaze {
+public class Hexamaze extends Widget {
     public static final Map<Color, Integer> COLOR_MAP = new HashMap<>();
     public static final Color PEG_COLOR = new Color(0.65, 0.65, 0.65, 1.0);
 
@@ -26,4 +40,40 @@ public class Hexamaze {
         COLOR_MAP.put(PINK, 5);
     }
 
+    public static Quartet<@NotNull Grid, @Nullable String, @Nullable Integer, @Nullable List<Coordinates>> solve(
+            @NotNull List<HexNode> nodeList) {
+        Maze maze = new Maze();
+        Grid original = new Grid(new HexagonalPlane(nodeList));
+
+        Grid found = MazeSearch.search(maze, original);
+        if (found == null)
+            throw new IllegalArgumentException("Could not find maze from given shapes");
+
+        int colorValue = copyPegLocation(original, found);
+        Pair<String, List<Coordinates>> exitInfo = ExitChecker.findPossibleExits(found);
+        if (exitInfo == null)
+            return new Quartet<>(found, null, null, null);
+
+        return new Quartet<>(
+                found,
+                exitInfo.getValue0(),
+                colorValue,
+                MazeRunner.runMaze(found, exitInfo.getValue1())
+        );
+    }
+
+    private static int copyPegLocation(Grid original, Grid found) {
+        List<HexNode> originalList = original.getHexagon().asList();
+        List<HexNode> foundList = found.getHexagon().asList();
+        int size = originalList.size();
+
+        for (int i = 0; i < size; i++) {
+            int currentNodeColorValue = originalList.get(i).getColor();
+            if (currentNodeColorValue != -1) {
+                foundList.get(i).setColor(currentNodeColorValue);
+                return currentNodeColorValue;
+            }
+        }
+        return -1;
+    }
 }
