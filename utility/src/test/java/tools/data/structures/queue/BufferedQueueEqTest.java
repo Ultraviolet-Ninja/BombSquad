8c1c715eb18bba07f44d14619cf22ab0d005d155
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-02-17 19:02:17 -0600

    Added some tests to the utility module

---
+++ utility/src/test/java/tools/data/structures/queue/BufferedQueueEqTest.java
@@ -0,0 +1,16 @@
+package tools.data.structures.queue;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.testng.annotations.Test;
+
+import static nl.jqno.equalsverifier.Warning.STRICT_INHERITANCE;
+
+public class BufferedQueueEqTest {
+    @Test(enabled = false)
+    public void equalsContract() {
+        EqualsVerifier.forClass(BufferedQueue.class)
+                .withIgnoredFields("capacity", "dataCache")
+                .suppress(STRICT_INHERITANCE)
+                .verify();
+    }
+}
